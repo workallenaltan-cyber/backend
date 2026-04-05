4e382825dcd147c44754f4d36f6821f73bdacd10
workallenaltan <workallenaltan@gmail.com>
2026-04-05 16:45:23 +0800

    update

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
 
 app.use(cors());
 app.use(express.json());
@@ -12,7 +17,3 @@ app.use(express.static("public"));
 // routes
 app.use("/api", require("./routes/auth"));
 app.use("/api", require("./routes/attendance"));
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
