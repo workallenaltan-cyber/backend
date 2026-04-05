1ee3bbb931a12e03ad5b57d8eecce7638b163c3c
workallenaltan <workallenaltan@gmail.com>
2026-04-05 12:36:29 +0800

    debug login

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const cors = require("cors");
 
 const app = express();
 
+app.post("/api/login", (req, res) => {
+  console.log("🔥 login route hit");
+
+  try {
+    const { employeeId, password } = req.body;
+    console.log("data:", employeeId, password);
+
+    return res.json({ status: "success", token: "abc123" });
+
+  } catch (err) {
+    console.error("❌ ERROR:", err);
+    res.status(500).json({ error: "server error" });
+  }
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
@@ -17,3 +32,4 @@ app.use("/api", require("./routes/admin"));
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+
