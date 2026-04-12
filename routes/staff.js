618eddfac7b3d77c4ca3e92b0e3ffc6ee06b019a
workallenaltan <workallenaltan@gmail.com>
2026-04-13 00:24:38 +0800

    update

--- routes/staff.js
+++ routes/staff.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../db");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 // =============================
