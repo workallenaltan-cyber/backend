448320c404e609df2c2a2d64b862d455d1d064e5
workallenaltan <workallenaltan@gmail.com>
2026-04-12 23:56:00 +0800

    update

--- public/app.js
+++ public/app.js
@@ -420,7 +420,7 @@ function loadStaff() {
   const token = localStorage.getItem("token");
   if (!token) return;
 
-  fetch(API + "/api/staff", {
+  fetch(API + "/api/loadstaff", {
     headers: {
       "Authorization": "Bearer " + token
     }
