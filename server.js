3b8e2ce7e4e91991d788192f08b63908bb342566
workallenaltan <workallenaltan@gmail.com>
2026-04-12 23:12:54 +0800

    update

--- server.js
+++ server.js
@@ -17,4 +17,4 @@ app.use(express.static("public"));
 // routes
 app.use("/api", require("./routes/auth"));
 app.use("/api", require("./routes/attendance"));
-app.use("/api", require("./routes/attendance"));
+app.use("/api", require("./routes/staff"));
